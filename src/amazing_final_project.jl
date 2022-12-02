29d4a2fb0e80de0a3cc19813b47b8c2702671c82
Zijiaa <liu_zj0705@163.com>
2022-12-01 22:22:12 -0800

    add test

--- src/amazing_final_project.jl
+++ src/amazing_final_project.jl
@@ -1,5 +1,7 @@
 module amazing_final_project
 
-# Write your package code here.
+export greet_your_package_name
+include("functions.jl")
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using amazing_final_project
 using Test
 
-@testset "amazing_final_project.jl" begin
-    # Write your tests here.
-end
+
+@testset "YourPackageName.jl" begin
+    @test YourPackageName.greet_your_package_name() == "Hello YourPackageName!"
+    @test YourPackageName.greet_your_package_name() != "Hello world!"
+end    # Write your tests here.
+
