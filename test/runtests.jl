27b3ce18a5a4b5251e683f1b48d119772327379a
Zijiaa01~ <liu_zj0705@163.com>
2022-12-12 17:37:42 -0800

    try to add codecov

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using amazing_final_project
 using Test
 
-
+include("test1.jl")
 @testset "YourPackageName.jl" begin
     @test YourPackageName.greet_your_package_name() == "Hello YourPackageName!"
     @test YourPackageName.greet_your_package_name() != "Hello world!"
